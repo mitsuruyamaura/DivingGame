29955a7c5b06a3aa8ecce5f38ff183cb2eb35c5c
mitsuru_yamaura <komayou326@gmail.com>
2020-11-27 18:04:55 +0900

    プレイヤーの飛び込みと着水処理、水面へ浮かぶ際のアニメ演出、カメラの追従と切り離し処理を実装。

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player;
+        
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (player.inWater == true) {
+            return;
+        }
+
+        if (player != null) {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float waterDrag;
+
+    public float moveSpeed;
+
+    public bool inWater;
+
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(180, 0, 0);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Debug.Log(x);
+        rb.velocity = new Vector3(x * moveSpeed, rb.velocity.y, z * moveSpeed);
+    }
+
+    private void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Water" && inWater == false) {
+            // エフェクト
+
+
+            rb.angularDrag = waterDrag;
+            //transform.eulerAngles = new Vector3(-30, 180, 0);
+            //rb.isKinematic = true;
+            inWater = true;
+
+            StartCoroutine(OutOfWater());
+            //transform.DORotate(Vector3.zero, 1.0f);
+        }
+
+        if (col.gameObject.tag == "Flower") {
+            Debug.Log("ステージクリア");
+
+            Destroy(col.gameObject, 0.5f);
+
+            // エフェクト
+        }
+    }
+
+    private IEnumerator OutOfWater()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        rb.isKinematic = true;
+
+        transform.eulerAngles = new Vector3(-30, 180, 0);
+
+        transform.DOMoveY(0, 1.0f);
+    }
+}
